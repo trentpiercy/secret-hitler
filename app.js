06fca17fb9fcdeb5e6b50a9c28f1f07def3cb32e
Trent Piercy <mcpvpxie@gmail.com>
2018-04-08 00:28:17 -0500

    clarity updates

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var app = new Vue({
     show_rolecard: false,
     show_flip: false,
     show_main: false,
-    show_draw: true,
+    show_draw: false,
     show_fascist_win: false,
     show_liberal_win: false,
     show_examine_all: false,
@@ -14,6 +14,7 @@ var app = new Vue({
     show_examine2: false,
     show_shuffle: false,
     show_veto: false,
+    show_chancellor_confirm: false,
 
     enable_veto: false,
 
@@ -81,10 +82,12 @@ var app = new Vue({
 
     fascist_win: function() {
       this.show_draw = false
+      this.show_chancellor_confirm = false
       this.show_fascist_win = true
     },
     liberal_win: function() {
       this.show_draw = false
+      this.show_chancellor_confirm = false
       this.show_liberal_win = true
     },
 
@@ -164,7 +167,7 @@ var app = new Vue({
         this.policies_list.push(this.policies_list_temp[r])
         this.policies_list_temp.splice(r, 1)
       }
-      this.show_draw = true
+      this.show_chancellor_confirm = true
       this.show_shuffle = false
       this.num_policies_left = this.policies_list.length
     },
@@ -175,13 +178,18 @@ var app = new Vue({
       this.show_policy_1 = false
       this.show_policy_2 = false
       this.show_policy_3 = false
-      this.show_draw = true
+      this.show_chancellor_confirm = true
       this.discard_tracker = 0
       this.num_policies_discarded += 1
       if (this.policies_list.length < 3) {
         this.show_shuffle = true
         this.show_draw = false
       }
+    },
+
+    confirm_chancellor: function() {
+      this.show_chancellor_confirm = false
+      this.show_draw = true
     },
 
     draw_3: function() {
@@ -209,7 +217,7 @@ var app = new Vue({
         this.show_veto = true
       }
       if (this.discard_tracker == 2) {
-        this.show_draw = true
+        this.show_chancellor_confirm = true
         this.show_policies = false
         this.discard_tracker = 0
         if (this.show_policy_2 == false) {
@@ -228,7 +236,7 @@ var app = new Vue({
         this.show_veto = true
       }
       if (this.discard_tracker == 2) {
-        this.show_draw = true
+        this.show_chancellor_confirm = true
         this.show_policies = false
         this.discard_tracker = 0
         if (this.show_policy_1 == false) {
@@ -247,7 +255,7 @@ var app = new Vue({
         this.show_veto = true
       }
       if (this.discard_tracker == 2) {
-        this.show_draw = true
+        this.show_chancellor_confirm = true
         this.show_policies = false
         this.discard_tracker = 0
         if (this.show_policy_1 == false) {
@@ -291,6 +299,7 @@ var app = new Vue({
         this.show_examine_all = true
         this.show_examine1 = true
         this.show_draw = false
+        this.show_chancellor_confirm = false
         this.show_shuffle = false
       }
 
@@ -300,6 +309,7 @@ var app = new Vue({
           this.show_shuffle = false
         }
         this.show_draw = false
+        this.show_chancellor_confirm = false
         this.show_policies = false
         this.show_policy_1 = false
         this.show_policy_2 = false
@@ -317,7 +327,7 @@ var app = new Vue({
       if (this.num_policies_left < 3) {
         this.show_shuffle = true
       } else {
-        this.show_draw = true
+        this.show_chancellor_confirm = true
       }
     }
 
